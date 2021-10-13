43c4e28f6714edd520f9ca7176bd80dcf640c902
Naresh Bhatia <naresh.archfirst@gmail.com>
2021-10-12 20:59:53 -0400

    Implemented integration test

--- cypress/integration/checkout.spec.ts
+++ cypress/integration/checkout.spec.ts
@@ -1,20 +1,30 @@
-describe('Order page', function () {
-  it('allows creating an order from a menu', function () {
-    // Go to order page
+const address = {
+  "firstName": "John",
+  "lastName": "Smith",
+  "company": "Northeast Design",
+  "address": "100 Federal Street",
+  "city": "Boston",
+  "state": "MA",
+  "zip": "02114"
+};
+
+describe('Checkout workflow', function () {
+  it('allows user to place an order', function () {
+    // Go to the home page
     cy.visit('/');
 
-    // Verify that the correct title is rendered
-    cy.contains('Our Menu');
+    // Add 4 items to the cart
+    cy.get('[data-testid="product"]').contains('iMac').click();
+    cy.get('[data-testid="product"]').contains('MacBook Pro').click();
+    cy.get('[data-testid="product"]').contains('iPad').click();
+    cy.get('[data-testid="product"]').contains('Google Home Mini').click();
 
-    // Add 4 menu items to the order
-    cy.get('[data-testid="menu-item"]').contains('Avocado Toast').click();
-    cy.get('[data-testid="menu-item"]').contains('Margherita Pizza').click();
-    cy.get('[data-testid="menu-item"]').contains('Four Cheese Pasta').click();
-    cy.get('[data-testid="menu-item"]').contains('Tiramisu').click();
+    // Verify that there are 4 items in the cart
+    cy.get('[data-testid="order-items"] > tbody > tr').should('have.length', 4);
 
-    // Delete "Four Cheese Pasta" from the order
+    // Delete "MacBook Pro" from the cart
     cy.get('[data-testid="order-items"]')
-      .contains('Four Cheese Pasta')
+      .contains('MacBook Pro')
       .parent()
       .find('[data-testid="delete-button"]')
       .click();
@@ -26,6 +36,22 @@ describe('Order page', function () {
     cy.contains('Checkout').click();
 
     // Verify that we are on the checkout page
-    cy.get('h1').contains('Checkout');
+    cy.contains('Place your order');
+
+    // Fill in the address
+    cy.get('input[name="shippingAddress.firstName"]').type(address.firstName);
+    cy.get('input[name="shippingAddress.lastName"]').type(address.lastName);
+    cy.get('input[name="shippingAddress.company"]').type(address.company);
+    cy.get('input[name="shippingAddress.address"]').type(address.address);
+    cy.get('input[name="shippingAddress.city"]').type(address.city);
+    cy.get('input[name="shippingAddress.state"]').type(address.state);
+    cy.get('input[name="shippingAddress.zip"]').type(address.zip);
+
+    // Place the order
+    cy.contains('Place your order').click();
+
+    // Make sure order appears in the orders page, total should be 1,577.00
+    cy.contains('John Smith');
+    cy.contains('1,577.00');
   });
 });
